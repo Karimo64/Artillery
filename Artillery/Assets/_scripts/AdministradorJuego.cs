99978bc16f22f126227db9a90dad1ca13e84e419
Karimo64 <42246635+Karimo64@users.noreply.github.com>
2025-04-28 19:14:40 -0600

    Artillery Demo
    - Añadi un segundo nivel
    - Cuenta con menus
    - Cambios al Administrador
    - Implementacion de Paneles

--- Artillery/Assets/_scripts/AdministradorJuego.cs
+++ Artillery/Assets/_scripts/AdministradorJuego.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AdministradorJuego : MonoBehaviour
 {
     public static AdministradorJuego SingletonAdministradorJuego;
+    // public static int VelocidadBala = 30;
+    // public static int DisparosPorJuego = 3; 
+    // public static float VelocidadRotacion = 1;
     [SerializeField] private int velocidadBala = 30;
-    [SerializeField] private int disparosPorJuego = 10;
+    [SerializeField] private int disparosPorJuego = 3;
     [SerializeField] private float velocidadRotacion = 1;
+
+    //public GameObject CanvasGanar;
+    //public GameObject CanvasPerder;
+    public GameObject PanelGanar;
+    public GameObject PanelPerder;
+    public bool esUltimoNivel = false;
+
 
 
     public int VelocidadBala
@@ -28,7 +39,8 @@ public class AdministradorJuego : MonoBehaviour
         set { velocidadRotacion = value; }
     }
 
-    public void Awake() {
+    private void Awake() 
+    {
         if (SingletonAdministradorJuego == null)
         {
             SingletonAdministradorJuego = this;
@@ -36,6 +48,49 @@ public class AdministradorJuego : MonoBehaviour
         else 
         {
             Debug.LogError("Ya existe una instancia de esta clase");
+            Destroy(gameObject);
         }  
+    }
+
+    private void Update()
+    {
+        if (DisparosPorJuego <= 0 && !PanelPerder.activeSelf && !PanelGanar.activeSelf)
+        {
+            if (GameObject.FindGameObjectsWithTag("Bala").Length == 0)
+            {
+                PerderJuego();
+            }
+        }
+    }
+
+    public void GanarJuego() 
+    {
+        PanelGanar.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void PerderJuego() 
+    {
+        PanelPerder.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void SiguienteNivel()
+    {
+        Time.timeScale = 1f; // Reanuda el tiempo
+        int siguienteIndice = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(siguienteIndice);
+    }
+
+    public void RegresarAlMenu()
+    {
+        Time.timeScale = 1f; // Reanuda el tiempo
+        SceneManager.LoadScene("MenuPrincipal");
+    }
+
+    public void ReintentarNivel()
+    {
+        Time.timeScale = 1f; // Reanuda el tiempo
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
--- Artillery/Assets/_scripts/Canon.cs
+++ Artillery/Assets/_scripts/Canon.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Canon : MonoBehaviour
 {
@@ -15,42 +18,102 @@ public class Canon : MonoBehaviour
     private GameObject puntaCanon;
     private float rotacion;
     //int numDisparos = AdministradorJuego.DisparosPorJuego;
+
+    public CanonControls canonControls;
+    private InputAction apuntar;
+    private InputAction modificarFuerza;
+    private InputAction disparar;
+
+    public Slider sliderFuerza;
+    public TextMeshProUGUI textoDisparos;
+    public float fuerzaMin = 10f;
+    public float fuerzaMax = 50f;
+    private float fuerzaActual;
+
+    private void Awake()
+    {
+        canonControls = new CanonControls();
+    }
+
+    private void OnEnable()
+    {
+        apuntar = canonControls.Canon.Apuntar;
+        modificarFuerza = canonControls.Canon.ModificarFuerza;
+        disparar = canonControls.Canon.Disparar;
+        apuntar.Enable();
+        modificarFuerza.Enable();  
+        disparar.Enable();
+        disparar.performed += Disparar;
+    }
+
+    private void OnDisable()
+    {
+        apuntar.Disable();
+        modificarFuerza.Disable();
+        disparar.Disable();
+    }
     
     private void Start()
     {
         puntaCanon = transform.Find("PuntaCanon").gameObject;
         SonidoDisparo = GameObject.Find("SonidoDisparo");
         SourceDisparo = SonidoDisparo.GetComponent<AudioSource>();
+
+        if (sliderFuerza != null)
+        {
+            sliderFuerza.minValue = fuerzaMin;
+            sliderFuerza.maxValue = fuerzaMax;
+            fuerzaActual = sliderFuerza.value;
+        }
     }
     void Update()
     {
-        rotacion += Input.GetAxis("Horizontal") * AdministradorJuego.SingletonAdministradorJuego.VelocidadRotacion;
-        if (rotacion <= 90 && rotacion >= 0)
+        rotacion += apuntar.ReadValue<float>() * AdministradorJuego.SingletonAdministradorJuego.VelocidadRotacion;
+        rotacion = Mathf.Clamp(rotacion, 0, 90); // Limitar la rotación entre 0 y 90 grados
+        // rotacion += Input.GetAxis("Horizontal") * AdministradorJuego.SingletonAdministradorJuego.VelocidadRotacion;
+        // if (rotacion <= 90 && rotacion >= 0)
+        // {
+        //     transform.eulerAngles = new Vector3(rotacion, 90, 0.0f);
+        // }
+
+        // if (rotacion > 90) rotacion = 90;
+        // if (rotacion < 0) rotacion = 0;
+        transform.eulerAngles = new Vector3(rotacion, 90, 0.0f);
+        float inputFuerza = modificarFuerza.ReadValue<float>();
+        if (sliderFuerza != null)
         {
-            transform.eulerAngles = new Vector3(rotacion, 90, 0.0f);
+            sliderFuerza.value = Mathf.Clamp(sliderFuerza.value + inputFuerza * Time.deltaTime * 10f, fuerzaMin, fuerzaMax);
+            fuerzaActual = sliderFuerza.value;
         }
-
-        if (rotacion > 90) rotacion = 90;
-        if (rotacion < 0) rotacion = 0;
-
-        if (Input.GetKeyDown(KeyCode.Space) && AdministradorJuego.SingletonAdministradorJuego.DisparosPorJuego > 0 && !Bloqueado)
+        if (AdministradorJuego.SingletonAdministradorJuego != null)
         {
-            GameObject temp = Instantiate(BalaPrefab, puntaCanon.transform.position, transform.rotation);
-            
-            Rigidbody tempRB = temp.GetComponent<Rigidbody>();
-            SeguirCamara.objetivo = temp;
-            Vector3 direccionDisparo = transform.rotation.eulerAngles;
-            direccionDisparo.y = 90 - direccionDisparo.x;
-            Vector3 direccionParticulas = new Vector3(-90 + direccionDisparo.x, 90, 0);
-            GameObject Particulas = Instantiate(ParticulasDisparo, puntaCanon.transform.position, Quaternion.Euler(direccionParticulas), transform);
-            tempRB.velocity = direccionDisparo.normalized * AdministradorJuego.SingletonAdministradorJuego.VelocidadBala;
-            AdministradorJuego.SingletonAdministradorJuego.DisparosPorJuego--;
-            Debug.Log("El numero de disoparos es: " + AdministradorJuego.SingletonAdministradorJuego.DisparosPorJuego); // Deja saber el numero de disparos restantes
-            //SourceDisparo.PlayOneShot(clipDisparo); //Este sonido solo se repoducira directamente en el objeto
-            SourceDisparo.Play(); //Este sonido se reproducira en el objeto y en el prefab
-            Bloqueado = true; //Desbloquea el disparo
+            textoDisparos.text = "Disparos: " + AdministradorJuego.SingletonAdministradorJuego.DisparosPorJuego;
         }
+        
 
+    }
 
+    private void Disparar(InputAction.CallbackContext context)
+    {
+        if (Bloqueado) return; // Si el disparo está bloqueado, no hacemos nada
+        if (AdministradorJuego.SingletonAdministradorJuego.DisparosPorJuego <= 0) return; // Si no hay disparos restantes, no hacemos nada
+
+        GameObject temp = Instantiate(BalaPrefab, puntaCanon.transform.position, transform.rotation);
+
+        Rigidbody tempRB = temp.GetComponent<Rigidbody>();
+        SeguirCamara.objetivo = temp;
+        Vector3 direccionDisparo = transform.rotation.eulerAngles;
+        direccionDisparo.y = 90 - direccionDisparo.x;
+        Vector3 direccionParticulas = new Vector3(-90 + direccionDisparo.x, 90, 0);
+        GameObject Particulas = Instantiate(ParticulasDisparo, puntaCanon.transform.position, Quaternion.Euler(direccionParticulas), transform);
+        tempRB.velocity = direccionDisparo.normalized * fuerzaActual; // Actualizamos para que tome valor del slider
+        // tempRB.velocity = direccionDisparo.normalized * AdministradorJuego.SingletonAdministradorJuego.VelocidadBala;
+        // AdministradorJuego.DisparosPorJuego--;
+        AdministradorJuego.SingletonAdministradorJuego.DisparosPorJuego--;
+        // Debug.Log("El numero de disoparos es: " + AdministradorJuego.DisparosPorJuego);
+        Debug.Log("El numero de disparos restantes es: " + AdministradorJuego.SingletonAdministradorJuego.DisparosPorJuego); // Deja saber el numero de disparos restantes
+                                                                                                                    //SourceDisparo.PlayOneShot(clipDisparo); //Este sonido solo se repoducira directamente en el objeto
+        SourceDisparo.Play(); //Este sonido se reproducira en el objeto y en el prefab
+        Bloqueado = true; //Desbloquea el disparo
     }
 }
